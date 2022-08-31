11a5dfc1db98c104c9c46ae135f7b7a1df6c22a7
Rahul Dubey <rahuld7219@gmail.com>
2022-08-31 19:01:04 +0530

    initial project setup and add user and wallet services (modules)

---
+++ src/test/java/com/walletme/WalletmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walletme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
